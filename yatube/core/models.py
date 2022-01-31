6dc2bf5d682a318f6744f6d394c3cf318f48328e
Фарух Темиров <alrov777@yandex.ru>
2022-02-01 01:14:16 +0300

    123

---
+++ yatube/core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class CreatedModel(models.Model):
+    """Абстрактная модель. Добавляет дату создания."""
+    created = models.DateTimeField(
+        'Дата создания',
+        auto_now_add=True
+    )
